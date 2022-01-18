93e13650a4ae670759bbda69fabc6928adefc00a
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-18 16:03:07 -0300

    Api RestFull with Spring Boot

---
+++ src/test/java/br/com/greg/restfull/RestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.greg.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
